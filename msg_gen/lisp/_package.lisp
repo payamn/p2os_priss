74fd989f8ae1bd37960e477bc35048b7a35f9302
autolab <autolab@priss.autolab>
2017-08-22 18:56:13 -0700

    init

---
+++ msg_gen/lisp/_package.lisp
@@ -0,0 +1,23 @@
+(cl:defpackage p2os_driver-msg
+  (:use )
+  (:export
+   "<GRIPPERSTATE>"
+   "GRIPPERSTATE"
+   "<AIO>"
+   "AIO"
+   "<GRIPSTATE>"
+   "GRIPSTATE"
+   "<SONARARRAY>"
+   "SONARARRAY"
+   "<MOTORSTATE>"
+   "MOTORSTATE"
+   "<LIFTSTATE>"
+   "LIFTSTATE"
+   "<PTZSTATE>"
+   "PTZSTATE"
+   "<DIO>"
+   "DIO"
+   "<BATTERYSTATE>"
+   "BATTERYSTATE"
+  ))
+
